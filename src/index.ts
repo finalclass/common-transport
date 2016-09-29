144250565214564efd841338507cd7871068bb19
Szymon Wygnanski <s@finalclass.net>
2016-09-29 11:15:23 +0200

    adding npmignore

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-console.log('index');
+import * as SingleInit from 'single-init';
+
+export interface ICommonTransportConfig {
+    adapter:'amqp';
+    amqp:{
+        host:string;
+        port:string;
+        user:string;
+        pass:string;
+    };
+}
+
+export class CommonTransport {
+
+    constructor(private config:ICommonTransportConfig) {
+
+    }
+
+    public on(method:string, params:any[], reply:(data:any)=>void) {
+
+    }
+
+    public call(method:string, ...params:any[]):Promise<any> {
+        return new Promise((resolve, reject) => {
+
+        });
+    }
+
+}
